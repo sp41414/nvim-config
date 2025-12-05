7432960f223446f5033285d83af6b16a44f94e30
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-12-06 02:59:59 +0400

    build(deps): add new languages to mason-lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,14 @@ return {
 	{
 		"mason-org/mason-lspconfig.nvim",
 		opts = {
-			ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls" },
+			ensure_installed = {
+				"lua_ls", "rust_analyzer", "ts_ls",
+				"clangd", "gopls", "jdtls",
+				"cssls", "html", "jsonls",
+				"pyright", "prettierd",
+				"yaml_ls", "bashls", "marksman", "fish_lsp",
+				"tailwindcss"
+			},
 		},
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
