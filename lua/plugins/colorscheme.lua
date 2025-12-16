3e76dc7690646974658a889943d33675cbc0f864
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-12-16 15:54:16 +0400

    feat: add biome.js & tokyodark colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,38 @@
 return {
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				styles = {
-					bold = true,
-					italic = true,
-					transparency = false,
-				},
-				highlight_groups = {
-					Normal = { bg = "#000000" },
-					NormalNC = { bg = "#000000" },
-					NormalFloat = { bg = "#000000" },
-					FloatBorder = { fg = "subtle", bg = "#000000" },
-					FloatTitle = { bg = "#000000" },
-					DiagnosticFloatingError = { bg = "#000000" },
-					DiagnosticFloatingWarn = { bg = "#000000" },
-					DiagnosticFloatingInfo = { bg = "#000000" },
-					DiagnosticFloatingHint = { bg = "#000000" },
-				},
-			})
-
-			vim.cmd("colorscheme rose-pine")
-		end,
-	},
+    -- {
+    -- 	"rose-pine/neovim",
+    -- 	name = "rose-pine",
+    -- 	config = function()
+    -- 		require("rose-pine").setup({
+    -- 			styles = {
+    -- 				bold = true,
+    -- 				italic = true,
+    -- 				transparency = false,
+    -- 			},
+    -- 			highlight_groups = {
+    -- 				Normal = { bg = "#000000" },
+    -- 				NormalNC = { bg = "#000000" },
+    -- 				NormalFloat = { bg = "#000000" },
+    -- 				FloatBorder = { fg = "subtle", bg = "#000000" },
+    -- 				FloatTitle = { bg = "#000000" },
+    -- 				DiagnosticFloatingError = { bg = "#000000" },
+    -- 				DiagnosticFloatingWarn = { bg = "#000000" },
+    -- 				DiagnosticFloatingInfo = { bg = "#000000" },
+    -- 				DiagnosticFloatingHint = { bg = "#000000" },
+    -- 			},
+    -- 		})
+    --
+    -- 		vim.cmd("colorscheme rose-pine")
+    -- 	end,
+    -- },
+    {
+        "tiagovla/tokyodark.nvim",
+        opts = {
+            -- custom options here
+        },
+        config = function(_, opts)
+            require("tokyodark").setup(opts) -- calling setup is optional
+            vim.cmd [[colorscheme tokyodark]]
+        end,
+    }
 }
