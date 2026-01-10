05f3b4f7da6dc8ad87c6b2aed487c029ab6948e5
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-01-10 15:42:24 +0400

    feat: remove autopairs

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,114 +1,114 @@
 return {
-	{
-		"mbbill/undotree",
-		cmd = "UndotreeToggle",
-		config = function()
-			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-		end,
-	},
-	{
-		"folke/zen-mode.nvim",
-		cmd = "ZenMode",
-		keys = {
-			{ "<leader>z", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" },
-		},
-		opts = {
-			kitty = {
-				enabled = true,
-				font = "+4",
-			},
-			window = {
-				options = {
-					number = true,
-				},
-			},
-		},
-	},
-	{
-		"laytan/cloak.nvim",
-		config = function()
-			require("cloak").setup({
-				enabled = true,
-				cloak_character = "*",
-				-- The applied highlight group (colors) on the cloaking, see `:h highlight`.
-				highlight_group = "Comment",
-				-- Applies the length of the replacement characters for all matched
-				-- patterns, defaults to the length of the matched pattern.
-				cloak_length = nil, -- Provide a number if you want to hide the true length of the value.
-				-- Whether it should try every pattern to find the best fit or stop after the first.
-				try_all_patterns = true,
-				-- Set to true to cloak Telescope preview buffers. (Required feature not in 0.1.x)
-				cloak_telescope = true,
-				-- Re-enable cloak when a matched buffer leaves the window.
-				cloak_on_leave = false,
-				patterns = {
-					{
-						-- Match any file starting with '.env'.
-						-- This can be a table to match multiple file patterns.
-						file_pattern = ".env*",
-						-- Match an equals sign and any character after it.
-						-- This can also be a table of patterns to cloak,
-						-- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
-						cloak_pattern = "=.+",
-						-- A function, table or string to generate the replacement.
-						-- The actual replacement will contain the 'cloak_character'
-						-- where it doesn't cover the original text.
-						-- If left empty the legacy behavior of keeping the first character is retained.
-						replace = nil,
-					},
-				},
-			})
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-		opts = {},
-	},
-	{
-		"numToStr/Comment.nvim",
-		event = "VeryLazy",
-		opts = {},
-		keys = {
-			{ "gcc", mode = "n", desc = "Comment toggle current line" },
-			{ "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
-			{ "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
-		},
-	},
-	{
-		"kylechui/nvim-surround",
-		version = "*",
-		event = "VeryLazy",
-		opts = {},
-	},
-	-- {
-	-- 	"okuuva/auto-save.nvim",
-	-- 	event = { "InsertLeave", "TextChanged" },
-	-- 	opts = {},
-	-- },
-	{
-		"folke/todo-comments.nvim",
-		event = "VeryLazy",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
-	},
-	{
-		"vyfor/cord.nvim",
-		event = "VeryLazy",
-		build = ":Cord update",
-		-- opts = {},
-	},
-	{
-		"norcalli/nvim-colorizer.lua",
-		event = "BufReadPre",
-		config = function()
-			require("colorizer").setup({
-				"*",
-				user_default_options = {
-					mode = "background",
-				},
-			})
-		end,
-	},
+    {
+        "mbbill/undotree",
+        cmd = "UndotreeToggle",
+        config = function()
+            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+        end,
+    },
+    {
+        "folke/zen-mode.nvim",
+        cmd = "ZenMode",
+        keys = {
+            { "<leader>z", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" },
+        },
+        opts = {
+            kitty = {
+                enabled = true,
+                font = "+4",
+            },
+            window = {
+                options = {
+                    number = true,
+                },
+            },
+        },
+    },
+    {
+        "laytan/cloak.nvim",
+        config = function()
+            require("cloak").setup({
+                enabled = true,
+                cloak_character = "*",
+                -- The applied highlight group (colors) on the cloaking, see `:h highlight`.
+                highlight_group = "Comment",
+                -- Applies the length of the replacement characters for all matched
+                -- patterns, defaults to the length of the matched pattern.
+                cloak_length = nil, -- Provide a number if you want to hide the true length of the value.
+                -- Whether it should try every pattern to find the best fit or stop after the first.
+                try_all_patterns = true,
+                -- Set to true to cloak Telescope preview buffers. (Required feature not in 0.1.x)
+                cloak_telescope = true,
+                -- Re-enable cloak when a matched buffer leaves the window.
+                cloak_on_leave = false,
+                patterns = {
+                    {
+                        -- Match any file starting with '.env'.
+                        -- This can be a table to match multiple file patterns.
+                        file_pattern = ".env*",
+                        -- Match an equals sign and any character after it.
+                        -- This can also be a table of patterns to cloak,
+                        -- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
+                        cloak_pattern = "=.+",
+                        -- A function, table or string to generate the replacement.
+                        -- The actual replacement will contain the 'cloak_character'
+                        -- where it doesn't cover the original text.
+                        -- If left empty the legacy behavior of keeping the first character is retained.
+                        replace = nil,
+                    },
+                },
+            })
+        end,
+    },
+    -- {
+    -- 	"windwp/nvim-autopairs",
+    -- 	event = "InsertEnter",
+    -- 	config = true,
+    -- 	opts = {},
+    -- },
+    {
+        "numToStr/Comment.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            { "gcc", mode = "n",          desc = "Comment toggle current line" },
+            { "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
+            { "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
+        },
+    },
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        opts = {},
+    },
+    -- {
+    -- 	"okuuva/auto-save.nvim",
+    -- 	event = { "InsertLeave", "TextChanged" },
+    -- 	opts = {},
+    -- },
+    {
+        "folke/todo-comments.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {},
+    },
+    {
+        "vyfor/cord.nvim",
+        event = "VeryLazy",
+        build = ":Cord update",
+        -- opts = {},
+    },
+    {
+        "norcalli/nvim-colorizer.lua",
+        event = "BufReadPre",
+        config = function()
+            require("colorizer").setup({
+                "*",
+                user_default_options = {
+                    mode = "background",
+                },
+            })
+        end,
+    },
 }
