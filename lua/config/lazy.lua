887300131ec6d25dafa299a6a0efd273217a6798
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-01-14 17:22:57 +0400

    feat: update colorscheme tokyo.nvim oled

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -26,41 +26,41 @@ local autocmd = vim.api.nvim_create_autocmd
 
 -- LSP
 autocmd("LspAttach", {
-	group = TheGroup,
-	callback = function(e)
-		local opts = { buffer = e.buf }
-		vim.keymap.set("n", "gd", function()
-			vim.lsp.buf.definition()
-		end, opts)
-		vim.keymap.set("n", "K", function()
-			vim.lsp.buf.hover()
-		end, opts)
-		vim.keymap.set("n", "<leader>vws", function()
-			vim.lsp.buf.workspace_symbol()
-		end, opts)
-		vim.keymap.set("n", "<leader>vd", function()
-			vim.diagnostic.open_float()
-		end, opts)
-		vim.keymap.set("n", "<leader>vca", function()
-			vim.lsp.buf.code_action()
-		end, opts)
-		vim.keymap.set("n", "<leader>vrr", function()
-			vim.lsp.buf.references()
-		end, opts)
-		vim.keymap.set("n", "<leader>vrn", function()
-			vim.lsp.buf.rename()
-		end, opts)
-		vim.keymap.set("i", "<C-h>", function()
-			vim.lsp.buf.signature_help()
-		end, opts)
-	end,
+    group = TheGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function()
+            vim.lsp.buf.definition()
+        end, opts)
+        vim.keymap.set("n", "K", function()
+            vim.lsp.buf.hover()
+        end, opts)
+        vim.keymap.set("n", "<leader>vws", function()
+            vim.lsp.buf.workspace_symbol()
+        end, opts)
+        vim.keymap.set("n", "<leader>vd", function()
+            vim.diagnostic.open_float()
+        end, opts)
+        vim.keymap.set("n", "<leader>vca", function()
+            vim.lsp.buf.code_action()
+        end, opts)
+        vim.keymap.set("n", "<leader>vrr", function()
+            vim.lsp.buf.references()
+        end, opts)
+        vim.keymap.set("n", "<leader>vrn", function()
+            vim.lsp.buf.rename()
+        end, opts)
+        vim.keymap.set("i", "<C-h>", function()
+            vim.lsp.buf.signature_help()
+        end, opts)
+    end,
 })
 
 vim.diagnostic.config({
-	virtual_text = true,
-	signs = true,
-	underline = false,
-	float = { border = "rounded" },
+    virtual_text = true,
+    signs = true,
+    underline = false,
+    float = { border = "rounded" },
 })
 
 -- END
@@ -76,13 +76,13 @@ vim.g.loaded_gzip = 1
 
 -- Setup lazy.nvim
 require("lazy").setup({
-	spec = {
-		-- import your plugins
-		{ import = "plugins" },
-	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	-- install = { colorscheme = { "habamax" } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
+    spec = {
+        -- import your plugins
+        { import = "plugins" },
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    -- install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
 })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,38 +1,48 @@
 return {
     -- {
-    -- 	"rose-pine/neovim",
-    -- 	name = "rose-pine",
-    -- 	config = function()
-    -- 		require("rose-pine").setup({
-    -- 			styles = {
-    -- 				bold = true,
-    -- 				italic = true,
-    -- 				transparency = false,
-    -- 			},
-    -- 			highlight_groups = {
-    -- 				Normal = { bg = "#000000" },
-    -- 				NormalNC = { bg = "#000000" },
-    -- 				NormalFloat = { bg = "#000000" },
-    -- 				FloatBorder = { fg = "subtle", bg = "#000000" },
-    -- 				FloatTitle = { bg = "#000000" },
-    -- 				DiagnosticFloatingError = { bg = "#000000" },
-    -- 				DiagnosticFloatingWarn = { bg = "#000000" },
-    -- 				DiagnosticFloatingInfo = { bg = "#000000" },
-    -- 				DiagnosticFloatingHint = { bg = "#000000" },
-    -- 			},
-    -- 		})
+    --     "rose-pine/neovim",
+    --     name = "rose-pine",
+    --     config = function()
+    --         require("rose-pine").setup({
+    --             styles = {
+    --                 bold = true,
+    --                 italic = true,
+    --                 transparency = false,
+    --             },
+    --             highlight_groups = {
+    --                 Normal = { bg = "#000000" },
+    --                 NormalNC = { bg = "#000000" },
+    --                 NormalFloat = { bg = "#000000" },
+    --                 FloatBorder = { fg = "subtle", bg = "#000000" },
+    --                 FloatTitle = { bg = "#000000" },
+    --                 DiagnosticFloatingError = { bg = "#000000" },
+    --                 DiagnosticFloatingWarn = { bg = "#000000" },
+    --                 DiagnosticFloatingInfo = { bg = "#000000" },
+    --                 DiagnosticFloatingHint = { bg = "#000000" },
+    --             },
+    --         })
     --
-    -- 		vim.cmd("colorscheme rose-pine")
-    -- 	end,
+    --         vim.cmd("colorscheme rose-pine")
+    --     end,
     -- },
     {
-        "tiagovla/tokyodark.nvim",
-        opts = {
-            -- custom options here
-        },
-        config = function(_, opts)
-            require("tokyodark").setup(opts) -- calling setup is optional
-            vim.cmd [[colorscheme tokyodark]]
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                style = "night",
+                on_colors = function(colors)
+                    colors.bg = "#000000"
+                    colors.bg_dark = "#000000"
+                    colors.bg_float = "#000000"
+                    colors.bg_popup = "#000000"
+                    colors.bg_sidebar = "#000000"
+                    colors.bg_statusline = "#000000"
+                end,
+            })
+
+            vim.cmd("colorscheme tokyonight")
         end,
     }
 }
